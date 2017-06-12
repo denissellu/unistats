e1d6b54636eaabd2f6554ba31069cd4bb0232ac4
denissellu <hingasellu@gmail.com>
2017-06-12 21:21:55 +0100

    add kismode

--- lib/unistats.rb
+++ lib/unistats.rb
@@ -4,9 +4,9 @@ require 'httparty'
 class Unistats
   include HTTParty
   format :json
-  
+
   base_uri 'data.unistats.ac.uk/api/V3/KIS'
-  
+
   # Initialises the Unistats class and configures authentication with an
   # access token - obtain one at http://dataportal.unistats.ac.uk/Account/Register
   # Params:
@@ -14,7 +14,7 @@ class Unistats
   def initialize(access_token)
     self.class.basic_auth access_token, nil
   end
-  
+
   # Checks whether provided API authentication credentials for the Unistats
   # API are correct, returning *true* or *false*
   def authenticated?
@@ -26,7 +26,7 @@ class Unistats
       true
     end
   end
-  
+
   # Returns a list of institutions registered on Unistats in pages of 25
   # entries as an object parsed from JSON
   # Params:
@@ -35,7 +35,7 @@ class Unistats
     options.merge!({:query => {:pageIndex => pageIndex}})
     self.class.get("/Institutions.json", options)
   end
-  
+
   # Returns information about a specified institution as an object parsed
   # from JSON
   # Params:
@@ -43,7 +43,7 @@ class Unistats
   def institution(institution, options={})
     self.class.get("/Institution/#{institution}.json", options)
   end
-  
+
   # Returns information about courses at an institution as an object parsed
   # from JSON, in pages of 25 entries
   # Params:
@@ -53,7 +53,7 @@ class Unistats
     options.merge!({:query => {:pageIndex => pageIndex}})
     self.class.get("/Institution/#{institution}/Courses.json", options)
   end
-  
+
   # Returns information about a specific course at an institution as an object
   # parsed from JSON
   # Params:
@@ -61,8 +61,8 @@ class Unistats
   # +course+:: the KIS ID for a course, unique to a course provider (UKPRN)
   # +recursive+:: (boolean) sets whether to use the other course data methods
   # to find out more details about this course (e.g. statistics, stages)
-  def course(institution, course, recursive=true, options={})
-    response = self.class.get("/Institution/#{institution}/Course/#{course}.json", options)
+  def course(institution, course, kismode, recursive=true, options={})
+    response = self.class.get("/Institution/#{institution}/Course/#{course}/#{kismode}.json", options)
     if recursive
       response.merge!({:accreditations => course_accreditations(institution, course)})
       response.merge!({:stages => course_stages(institution, course)})
@@ -70,7 +70,7 @@ class Unistats
     end
     response
   end
-  
+
   # Returns information on accreditations for a specific course at an
   # institution as an object parsed from JSON
   # Params:
@@ -79,7 +79,7 @@ class Unistats
   def course_accreditations(institution, course, options={})
     self.class.get("/Institution/#{institution}/Course/#{course}/Accreditation.json", options)
   end
-  
+
   # Returns information on stages of a specific course at an
   # institution as an object parsed from JSON
   # Params:
@@ -88,7 +88,7 @@ class Unistats
   def course_stages(institution, course, options={})
     self.class.get("/Institution/#{institution}/Course/#{course}/Stages.json", options)
   end
-  
+
   # Returns statistics aboout a specific course at an institution as an object
   # parsed from JSON
   # Params:
@@ -98,5 +98,5 @@ class Unistats
     self.class.get("/Institution/#{institution}/Course/#{kiscourseid}/#{kismode}.json", options)
   end
 end
-    
-    
+
+
